89158a63875d7a66c5cdc9c11f260967dd5e8984
randy riback <randolph.riback@gmail.com>
2021-07-28 15:57:07 -0400

    plz7

--- src/mock/data.js
+++ src/mock/data.js
@@ -33,7 +33,7 @@ export const projectsData = [
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/randyriback', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
