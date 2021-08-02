ce604335c3c5e6787ce6e8b81e99905ff6e64489
randy riback <randolph.riback@gmail.com>
2021-08-02 16:11:36 -0400

    bc2

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -39,14 +39,14 @@ const About = () => {
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
                   {paragraphOne ||
-                    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                    "With a background in full-stack development, hospitality, music, and customer service, I'm interested in building lasting systems that take companies to their highest potential"}
                 </p>
                 <p className="about-wrapper__info-text">
                   {paragraphTwo ||
                     'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'}
+                  {paragraphThree || 'Technologies / Languages \nBack End: Flask, PostgreSQL, Firebase \nFront End: React.js, JavaScript, HTML5, CSS3, Bootstrap \nOther: Python, Git'}
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
