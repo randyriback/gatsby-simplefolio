51c6df67f3cbb6c04154750e4e2eaf9df19b78ca
randy riback <randolph.riback@gmail.com>
2021-08-02 21:48:05 -0400

    bc8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,23 @@ import Projects1 from './Projects/Projects1';
 import Contact from './Contact/Contact';
 import { PortfolioProvider } from '../context/context';
 
-import { aboutData, projectsData, contactData, footerData } from '../mock/data';
+import { aboutData, projectsData, projectsData1, contactData, footerData } from '../mock/data';
 
 function App() {
   const [about, setAbout] = useState({});
   const [projects, setProjects] = useState([]);
+  const [projects1, setProjects1] = useState([]);
   const [contact, setContact] = useState({});
 
   useEffect(() => {
     setAbout({ ...aboutData });
     setProjects([...projectsData]);
+    setProjects1([...projectsData1]);
     setContact({ ...contactData });
   }, []);
 
   return (
-    <PortfolioProvider value={{ about, projects, projects, contact}}>
+    <PortfolioProvider value={{ about, projects, projects1, contact}}>
       <About />
       <Projects />
       <Projects1 />
