1e1ca491b9b8b894e82b2703f56e059102431ce9
randy riback <randolph.riback@gmail.com>
2021-08-03 00:47:29 -0400

    bc8

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -59,9 +59,12 @@ const About = () => {
           </Col>
         </Row>
         <h3>Projects</h3>
+        <span>
         <Link to="projects" smooth duration={1000}>
             <i className="fa fa-angle-down fa-2x" aria-hidden="true" />
           </Link>
+          </span>
+        
       </Container>
     </section>
   );
