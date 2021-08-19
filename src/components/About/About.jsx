c399e97dce8018ce94226c29a6d91d3ebe0b0df7
randy riback <randolph.riback@gmail.com>
2021-08-19 19:17:22 -0400

    adding arro22

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
                     
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Proficient in Flask, React.js, PostgreSQL, Python, JavaScript, HTML5, CSS3, Firebase, Gatsby, Heroku, Git, Ableton Live, Protools'}
+                  {paragraphThree || 'Skills include Flask, React.js, PostgreSQL, Python, JavaScript, HTML5, CSS3, Firebase, Gatsby, Heroku, Git, Ableton Live, Protools'}
                 </p>
                 <p className="about-wrapper__info-text">
                 <a href="mailto:randolph.riback@gmail.com">Say hello</a> or find on <a href="https://github.com/randyriback">GitHub</a>, <a href="https://www.linkedin.com/in/randolph-riback/">LinkedIn</a>, and <a href="https://www.instagram.com/_track87/">Instagram</a>.
