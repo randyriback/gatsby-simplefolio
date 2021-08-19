970b5a71e46ba0a9f4676c97984f79923b6813e6
randy riback <randolph.riback@gmail.com>
2021-08-19 14:02:50 -0400

    add music3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import About from './About/About';
 import Projects from './Projects/Projects';
+import Music from './Music/Music';
 import Contact from './Contact/Contact';
 import { PortfolioProvider } from '../context/context';
 
