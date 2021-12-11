2c8870126e6fd02bb77c9bc7af9e5d9c75f40a25
randy riback <randolph.riback@gmail.com>
2021-12-11 00:33:27 -0500

    new designz

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,12 +5,11 @@ import { Link } from 'react-scroll';
 import Title from '../Title/Title';
 import AboutImg from '../Image/AboutImg';
 import PortfolioContext from '../../context/context';
-
-
+import musicfile from '../../images/wav-file.png';
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
-  const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
+  const { img, paragraphTwo, paragraphThree, resume } = about;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -40,45 +39,64 @@ const About = () => {
           <Col md={6} sm={12}>
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
+                <a href="https://www.soundcloud.com/jungdeejay" target="_blank" rel="noreferrer">
+                  {' '}
+                  <img src={musicfile} id="file" alt="tunes" />{' '}
+                </a>
+                <a href="https://www.soundcloud.com/jungdeejay" target="_blank" rel="noreferrer">
+                  {' '}
+                  <img src={musicfile} id="file2" alt="tunes" />{' '}
+                </a>
+                <a href="https://www.soundcloud.com/jungdeejay" target="_blank" rel="noreferrer">
+                  {' '}
+                  <img src={musicfile} id="file3" alt="tunes" />{' '}
+                </a>
                 <p className="about-wrapper__info-text">
-                    <strong>Hi, I'm Randy Riback. I'm a full-stack software developer, <a href="https://soundcloud.com/jung_deejay">artist</a>, and natural wine enthusiast based in Beacon, NY. I run an independent cassette series called <a href="https://kaphaselections.bandcamp.com">Kapha Selections</a> and am currently a fellow at the <a href="https://www.recurse.com/">Recurse Center</a>.</strong>
+                  <strong>
+                    Hi, I&apos;m Randy Riback. I&apos;m a full-stack software developer,{' '}
+                    <a href="https://soundcloud.com/jungdeejay">artist</a>, and natural wine
+                    enthusiast based in Beacon, NY. I run an independent cassette series called{' '}
+                    <a href="https://kaphaselections.bandcamp.com">Kapha Selections</a> and am
+                    currently a fellow at the <a href="https://www.recurse.com/">Recurse Center</a>.
+                  </strong>
+                </p>
+                <p className="about-wrapper__info-text">{paragraphTwo || ''}</p>
+                <p className="about-wrapper__info-text">
+                  {paragraphThree ||
+                    "I'm proficient with Flask, React.js, Node.js, Express, GraphQL, PostgreSQL, Python, JavaScript, HTML5, CSS3, Firebase, Gatsby, Heroku, & Git."}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphTwo ||
-                    ""}
-                    
-                </p>
-                <p className="about-wrapper__info-text">
-                  {paragraphThree || 'Proficient in Flask, React.js, PostgreSQL, Python, JavaScript, HTML5, CSS3, Firebase, Gatsby, Heroku, Git, Ableton Live & Protools'}
-                </p>
-                <p className="about-wrapper__info-text">
-                <a href="mailto:randolph.riback@gmail.com">Say hello</a> or find me on <a href="https://github.com/randyriback">GitHub</a> and <a href="https://www.linkedin.com/in/randolph-riback/">LinkedIn</a>.
+                  <a href="mailto:randolph.riback@gmail.com">Say hello</a> or find me on{' '}
+                  <a href="https://github.com/randyriback">GitHub</a> and{' '}
+                  <a href="https://www.linkedin.com/in/randolph-riback/">LinkedIn</a> :)
                 </p>
                 <span className="d-flex mt-3">
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="cta-btn cta-btn--resume"
-                      href={resume}
-                    >
-                      View Resume
-                    </a>
-                  </span>
-        
-            
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="cta-btn cta-btn--resume"
+                    href={resume}
+                  >
+                    View Resume
+                  </a>
+                </span>
               </div>
             </Fade>
           </Col>
         </Row>
-          </Container>
-          <div id="done">
-      <h3>Coding Projects</h3>
+      </Container>
+      <div id="done">
+        <h3>Coding Projects</h3>
         <span>
-        <Link to="projects" smooth duration={1000}>
-            <i className="fa fa-angle-down fa-2x" aria-hidden="true" style={{ cursor: "pointer" }}/>
+          <Link to="projects" smooth duration={1000}>
+            <i
+              className="fa fa-angle-down fa-2x"
+              aria-hidden="true"
+              style={{ cursor: 'pointer' }}
+            />
           </Link>
-          </span>
-          </div>
+        </span>
+      </div>
     </section>
   );
 };
